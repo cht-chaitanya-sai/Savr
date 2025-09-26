3d0cf1c2aeda01e7a9f0c93c821d6f5b3d850753
Rohan Kishore <rohankishore746@outlook.com>
2025-09-27 02:54:26 +0530

    #2 init

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -28,35 +28,6 @@ class Restaurant(models.Model):
         return self.name
 
 
-class Donation(models.Model):
-    """
-    Represents a single food donation listed by a restaurant.
-    """
-    STATUS_CHOICES = [
-        ('Ld', 'Listed'),
-        ('Clmd', 'Claimed'),
-        ('PkUp', 'Picked Up'),
-    ]
-
-    # Links to the restaurant that made the donation. If the restaurant is deleted, their donations are too.
-    rest = models.ForeignKey(
-        Restaurant, on_delete=models.SET_NULL, blank=True, null=True)
-
-    # Details of the food item
-    dish = models.CharField(max_length=200)
-    qty = models.IntegerField(validators=[MinValueValidator(1)], help_text="Number of servings")
-
-    # Pickup and status information
-    pickup_datetime = models.DateTimeField()
-    status = models.CharField(max_length=4, choices=STATUS_CHOICES, default='Ld')
-
-    # Links to the NGO that claimed the donation. Can be empty if unclaimed.
-    claimed_ngo = models.ForeignKey(NGO, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.dish} from {self.rest.name} ({self.get_status_display()})"
-
-
 class CustomUser(AbstractUser):
     type = models.CharField(
         choices=[
