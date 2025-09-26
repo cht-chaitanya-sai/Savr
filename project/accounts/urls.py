49657c0e613fca6238fcdf28a62a19f1da87df89
aman333nolawz <amananudhiu@gmail.com>
2025-09-26 20:57:53 +0530

    Update

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("rest_signup/", views.RestSignUpView.as_view(), name="restaurant_signup"),
-    path("ngo_signup/", views.NGOSignUpView.as_view(), name="ngo_signup"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.logoutView, name="logout"),
-    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
-]
+    path('rest_signup/', views.RestSignUpView.as_view(), name="restaurant_signup"),
+    path('ngo_signup/', views.NGOSignUpView.as_view(), name="ngo_signup"),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('dashboard/', views.DashboardView.as_view(), name="dashboard"),
 
+]
