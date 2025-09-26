fe36a52d88f586f5427ededcebe15df396bcffe9
Rohan Kishore <rohankishore746@outlook.com>
2025-09-26 22:14:33 +0530

    beautify

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -4,28 +4,33 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class NGO(models.Model):
-    name=models.CharField(max_length=100)
-    location=models.TextField()
-    email=models.EmailField()
-    ngoid=models.IntegerField()
-    profile=models.ImageField(upload_to="ngo/", null=True, blank=True)
+    name = models.CharField(max_length=100)
+    location = models.TextField()
+    email = models.EmailField()
+    ngoid = models.IntegerField()
+    profile = models.ImageField(upload_to="ngo/", null=True, blank=True)
 
 
 class Restaurant(models.Model):
-    name=models.CharField(max_length=100)
-    location=models.TextField()
-    email=models.EmailField()
-    phone=models.IntegerField(validators=[MinValueValidator(0000000000), MaxValueValidator(9999999999)])
-    fssai=models.IntegerField()
-    profile=models.ImageField(upload_to="rest/", null=True, blank=True)
+    name = models.CharField(max_length=100)
+    location = models.TextField()
+    email = models.EmailField()
+    phone = models.IntegerField(
+        validators=[MinValueValidator(0000000000), MaxValueValidator(9999999999)]
+    )
+    fssai = models.IntegerField()
+    profile = models.ImageField(upload_to="rest/", null=True, blank=True)
 
 
 class CustomUser(AbstractUser):
-    type=models.CharField(choices = [
-            ('NGO', 'NGO'),
-            ('Rest', 'Restaurant'),
+    type = models.CharField(
+        choices=[
+            ("NGO", "NGO"),
+            ("Rest", "Restaurant"),
         ]
     )
 
     ngo = models.ForeignKey(NGO, on_delete=models.SET_NULL, blank=True, null=True)
-    rest = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, blank=True, null=True)
+    rest = models.ForeignKey(
+        Restaurant, on_delete=models.SET_NULL, blank=True, null=True
+    )
