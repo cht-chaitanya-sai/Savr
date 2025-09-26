f69d7666e5d26670014f923713b0e53fe80b89b8
Rohit Joe Mendus <rohitmendus@gmail.com>
2025-09-26 18:00:07 +0530

    Added Database User TAbles

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class NGO(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.TextField()
+    email=models.EmailField()
+    ngoid=models.IntegerField()
+    profile=models.ImageField(upload_to="ngo/")
+
+
+class Restaurant(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.TextField()
+    email=models.EmailField()
+    phone=models.IntegerField(validators=[MinValueValidator(0000000000), MaxValueValidator(9999999999)])
+    fssai=models.IntegerField()
+    profile=models.ImageField(upload_to="rest/")
+
+
+class CustomUser(AbstractUser):
+    type=models.CharField(choices = [
+            ('NGO', 'NGO'),
+            ('Rest', 'Restaurant'),
+        ]
+    )
+
+    ngo = models.ForeignKey(NGO, blank=True, null=True)
+    rest = models.ForeignKey(Restaurant, blank=True, null=True)
--- project/project/settings.py
+++ project/project/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
