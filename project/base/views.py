648edd8ebd49d50fd3f1945d3480e8c09b67b7af
Rohan Kishore <rohankishore746@outlook.com>
2025-09-26 16:50:47 +0530

    reformat

--- project/base/views.py
+++ project/base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
+
 # Create your views here.
 class MainPageView(View):
-    template="base/main.html"
+    template = "base/main.html"
 
     def get(self, request):
-        return render(request, self.template,)
+        return render(request, self.template, )
