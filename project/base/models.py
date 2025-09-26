fd160f3008cf1ef53393e579130753f05a336231
Rohit Joe Mendus <rohitmendus@gmail.com>
2025-09-26 19:31:08 +0530

    Added Orders Model

--- project/base/models.py
+++ project/base/models.py
@@ -5,3 +5,5 @@ from accounts.models import Restaurant
 class Orders(models.Model):
     dish=models.CharField(max_length=100)
     qty=models.IntegerField()
+    rest=models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    pickup_time=models.TimeField()
